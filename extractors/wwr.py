b174a80f8e3940ed1d2eed305da23f8c7ebd024c
segene99 <segene99@gmail.com>
2023-06-26 00:37:17 +0900

    webscrapper

--- extractors/wwr.py
+++ extractors/wwr.py
@@ -2,7 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 
 
-def extractor_job(keyword):
+def extract_jobs_weworkremotely(keyword):
     base_url = "https://weworkremotely.com/remote-jobs/search?search_uuid=&term="
 
     response = get(f"{base_url}{keyword}")
@@ -20,15 +20,17 @@ def extractor_job(keyword):
                 anchors = post.find_all("a")
                 anchor = anchors[1]
                 link = anchor["href"]
-                company, kind, region = anchor.find_all("span", class_="company")
-                title = anchor.find("span", class_="title")
+                company, position, location = anchor.find_all("span", class_="company")
+                position = anchor.find("span", class_="title")
                 job_data = {
                     "link": f"https://weworkremotely.com/{link}",
                     "company": company.string,
-                    "region": region.string,
-                    "position": title.string,
+                    "location": location.string,
+                    "position": position.string,
                 }
                 results.append(job_data)
         for result in results:
             print(result)
             print("///////////////")
+
+extract_jobs_weworkremotely("python")
